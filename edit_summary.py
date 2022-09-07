5f07ad9c4982fac93663cb4dc837d01f357954cf
SongDaeSun <backgom07@gmail.com>
2022-09-07 23:41:36 +0900

    done

---
+++ edit_summary.py
@@ -0,0 +1,37 @@
+import os
+
+def edit_summary(longform_name):
+    f = open(str(os.path.abspath(os.getcwd())) +  "/longform_index_dir.txt", 'r')
+    target_dir = f.read()
+    f.close()
+
+    file_dir = target_dir + longform_name + "/" + longform_name + " 요약본.md"
+
+    f = open(file_dir, 'r')
+    summary_lines = f.readlines()
+    f.close()
+
+    found_title = False
+    
+    editted_summary = ""
+
+    for summary_line in summary_lines:
+        if not found_title:
+            if summary_line[0:2] == "# ":
+                found_title = True
+            editted_summary += summary_line
+        
+        else:
+            if summary_line[0:4] == "----":
+                found_title = False
+                editted_summary += summary_line
+            
+            if summary_line[0:2] == "# ":
+                editted_summary += summary_line
+        
+    f = open(file_dir, 'w')
+    f.write(editted_summary)
+    f.close()
+
+if __name__ == "__main__":
+   edit_summary("거시경제이론")
